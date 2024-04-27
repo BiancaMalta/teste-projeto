2aea05a770f6ab524f3f602c88831a32c113db98
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-04-27 11:53:52 -0300

    Update setup_database.sql

--- banco-de-dados/setup_database.sql
+++ banco-de-dados/setup_database.sql
@@ -1,5 +1,5 @@
 -- Concede permissões de leitura e escrita para o usuário 'ada' no banco de dados 'arquivos'
-GRANT SELECT, INSERT ON arquivos.* TO 'ada'@'localhost';
+GRANT SELECT, INSERT ON arquivos.* TO 'ada'@'%';
 
 -- Recarrega as permissões para que as alterações tenham efeito
 FLUSH PRIVILEGES;
