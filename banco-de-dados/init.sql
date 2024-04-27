c8ef8a32af1636e1d6b0236247e4d46ce99977bb
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-04-27 09:42:23 -0300

    Update init.sql

--- banco-de-dados/init.sql
+++ banco-de-dados/init.sql
@@ -9,11 +9,11 @@ CREATE TABLE IF NOT EXISTS Report (
     id INT AUTO_INCREMENT PRIMARY KEY,
     filename VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (filename)  -- Adiciona um índice único no campo filename
 );
 
 -- Insere alguns dados de exemplo na tabela de relatórios
 INSERT INTO Report (filename, username) VALUES ('relatorio1.pdf', 'admin');
 INSERT INTO Report (filename, username) VALUES ('relatorio2.pdf', 'usuario1');
 INSERT INTO Report (filename, username) VALUES ('relatorio3.pdf', 'usuario2');
-
