f28fefd9b33e5074e55445cd625c7a088bc0f8d3
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2024-04-27 11:56:27 -0300

    Update setup_database.sql

--- banco-de-dados/setup_database.sql
+++ banco-de-dados/setup_database.sql
@@ -1,4 +1,8 @@
--- Concede permissões de leitura e escrita para o usuário 'ada' no banco de dados 'arquivos'
+-- Cria o usuário 'ada' se ele ainda não existir
+CREATE USER IF NOT EXISTS 'ada'@'%';
+
+-- Concede todos os privilégios para o usuário 'ada' em todas as bases de dados e tabelas
 GRANT ALL PRIVILEGES ON *.* TO 'ada'@'%' WITH GRANT OPTION;
--- Recarrega as permissões para que as alterações tenham efeito
+
+-- Atualiza as permissões
 FLUSH PRIVILEGES;
